ca85cbde1b40cf02afed4d661bb166ae4c786d37
ASHA SHIVDASANI <shivdasaniasha@gmail.com>
2019-11-08 11:47:46 +0530

    Initial Commit

---
+++ CatalogAPI/Infrastructure/CatalogContext.cs
@@ -0,0 +1,47 @@
+﻿using CatalogAPI.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+
+namespace CatalogAPI.Infrastructure
+{
+    public class CatalogContext
+    {
+        private IConfiguration configuration;
+        private IMongoDatabase mongoDatabase; 
+        public CatalogContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            var conStr = configuration.GetValue<string>("MongoSettings:ConnectionStrings");
+
+            //MongoClientSettings settings  =  MongoClientSettings.FromConnectionString(conStr);
+   
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+              new MongoUrl(conStr)
+            );
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+          
+            MongoClient mongoClient = new MongoClient(settings);
+
+            if(mongoClient != null)
+            {
+                this.mongoDatabase = mongoClient.GetDatabase(configuration.GetValue<string>("MongoSettings:Database"));
+            }
+                            //data
+        }
+
+        public IMongoCollection<CatalogItem> Catalog
+        {
+            get
+            {
+                return this.mongoDatabase.GetCollection<CatalogItem>("products");
+            }
+
+        }
+    }
+}
---
+++ CatalogAPI/Startup.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CatalogAPI.CustomFormatters;
+using CatalogAPI.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CatalogAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddScoped<CatalogContext>();
+
+            services.AddCors(c =>
+            {
+                //c.AddDefaultPolicy(x => x.AllowAnyOrigin()
+                //.AllowAnyMethod()
+                //.AllowAnyHeader());
+
+                c.AddPolicy("AllowPartners", x =>
+                {
+                    x.WithOrigins("http://microsoft.com", "http://synergetic.com")
+                    .WithHeaders("GET", "POST")
+                    .AllowAnyHeader();
+                }
+                );
+
+                c.AddPolicy("AllowAll", x =>
+                {
+                    x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                }
+                );
+            });
+
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title="Catalog API",
+                    Description="Catalog management API",
+                    Version="1.0",
+                    Contact= new Contact
+                    {
+                        Name="Kiran",
+                        Email="Kiran@hexaware.com",
+                        Url="https;//github.com/kiran"
+                    }
+                });
+            });
+
+            services.AddAuthentication(c =>
+            {
+                c.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                c.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(c=> {
+                    c.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = true,
+                        ValidateIssuer = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration.GetValue<string>("Jwt:issuer"),
+                        ValidAudience = Configuration.GetValue<string>("Jwt:audience"),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Jwt:secret")))
+                    };
+                });
+
+            services.AddMvc(config =>
+            {
+                config.OutputFormatters.Add(new CsvOutPutFormatter());
+            })
+                .AddXmlDataContractSerializerFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            
+            //app.UseCors();
+            //app.UseCors("AllowPartners");
+            app.UseCors("AllowAll");
+
+            app.UseSwagger();
+            if (env.IsDevelopment())
+            {
+                app.UseSwaggerUI(config =>
+                {
+                    config.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API");
+                    config.RoutePrefix = "";
+                });
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
